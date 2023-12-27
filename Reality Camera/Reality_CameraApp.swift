a46fd83d405ffaecf0aa864f70bf25e0f24e7239
Fadey <notchenkofaddey@gmail.com>
2023-12-27 21:41:45 +0300

    Initial Commit

---
+++ Reality Camera/Reality_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reality_CameraApp.swift
+//  Reality Camera
+//
+//  Created by Fadey Notchenko on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Reality_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
